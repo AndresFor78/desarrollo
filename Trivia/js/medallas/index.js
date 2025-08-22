70801fb5ca84f80f5d5b6308773e7848a9f2c7a5
AndresFor78 <81651908+AndresFor78@users.noreply.github.com>
2025-08-22 11:31:35 -0400

    Delete Trivia directory

--- Trivia/js/medallas/index.js
+++
@@ -1,4 +0,0 @@
-export  { Medalla } from './medalla.js';
-export  { MedallaOro } from './medallaOro.js';
-export  { MedallaPlata } from './medallaPlata.js';
-export  { MedallaBronce } from './medallaBronce.js';
